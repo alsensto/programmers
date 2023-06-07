6319909a60087917fe3be6da1161df65fac5a831
alsensto <jmg1301@gmail.com>
2023-06-07 21:53:16 +0900

    120803

---
+++ src/S120803.java
@@ -0,0 +1,13 @@
+public class S120803 {
+    public static void main(String[] args) {
+        System.out.println(solution(2,3));
+    }
+
+    private static int solution(int num1, int num2) {
+
+            int answer = 0;
+            answer = num1 - num2;
+            return answer;
+    }
+
+}
