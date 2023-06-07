5e94d750be923e250ad5db06765cc87c21f7a550
alsensto <jmg1301@gmail.com>
2023-06-07 22:15:26 +0900

    120802

---
+++ src/lv0/S120802.java
@@ -0,0 +1,11 @@
+package lv0;
+
+public class S120802 {
+    public static void main(String[] args) {
+        System.out.println(solution(2,3));
+
+    }
+    static int solution(int num1, int num2){
+        return num1 + num2;
+    }
+}
