e12b31bcba84b66925cde88d83c1389a05364d02
alsensto <jmg1301@gmail.com>
2023-06-07 22:04:53 +0900

    120810

---
+++ src/S120810.java
@@ -0,0 +1,9 @@
+public class S120810 {
+    public static void main(String[] args) {
+        System.out.println(solution(3,2));
+    }
+
+    private static int solution(int num1, int num2) {
+        return num1%num2;
+    }
+}
