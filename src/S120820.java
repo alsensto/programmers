0b1ed9cc0d3794ddc7dd931fc12820dd320cf797
alsensto <jmg1301@gmail.com>
2023-06-07 22:01:54 +0900

    120820

---
+++ src/S120820.java
@@ -0,0 +1,9 @@
+public class S120820 {
+    public static void main(String[] args) {
+        System.out.println(solution(40));
+    }
+
+    private static int solution(int age) {
+        return 2023-age;
+    }
+}
