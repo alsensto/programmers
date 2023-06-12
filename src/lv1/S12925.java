d7df7d34c4400d60d02ac7211ffddf89bcef9c2f
alsensto <jmg1301@gmail.com>
2023-06-12 18:27:51 +0900

    S12925

---
+++ src/lv1/S12925.java
@@ -0,0 +1,13 @@
+package lv1;
+public class S12925 {
+    public static void main(String[] args) {
+        System.out.println("-1234");
+
+    }
+
+    static int Solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+
+    }
+}
